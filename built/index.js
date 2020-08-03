f5d2b8a57d9cfae718ef76e407bba954de0cd6da
Kseniya Kardash <Kseniya.Kardash@coxautoinc.com>
2020-08-03 15:48:05 +0300

    Added built folder to remote

---
+++ built/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ImageViewerPropsDefine = exports.ImageViewer = void 0;
+var image_viewer_component_1 = require("./image-viewer.component");
+exports.ImageViewer = image_viewer_component_1.default;
+var image_viewer_type_1 = require("./image-viewer.type");
+Object.defineProperty(exports, "ImageViewerPropsDefine", { enumerable: true, get: function () { return image_viewer_type_1.Props; } });
+exports.default = image_viewer_component_1.default;
+//# sourceMappingURL=index.js.map
